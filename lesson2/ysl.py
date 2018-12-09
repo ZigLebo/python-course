32b42ccdc7eb202d0b0cd387212e718f0174fcdc
Росс <AlikSubb@mail.ru>
2018-12-09 18:04:16 +0300

    l2 1

---
+++ lesson2/ysl.py
@@ -0,0 +1,25 @@
+print("Vvedite chislo")
+a=int(input())
+if a%2==0:
+  print(a,"chetnoe chislo")
+else: print(b, "nechotnoe chislo")
+print("Vvedite chislo")
+b=int(input())
+if b%2==0:
+  print(b,"chetnoe chislo")
+else: print(b, "nechotnoe chislo")
+print("Vvedite chislo")
+c=int(input())
+if b%2==0:
+  print(c,"chetnoe chislo")
+else: print(c, "nechotnoe chislo")
+print("Vvedite chislo")
+d=int(input())
+if d%2==0:
+  print(d, "chetnoe chislo")
+else: print(d, "nechotnoe chislo")
+print("Vvedite chislo")
+e=int(input())
+if e%2==0:
+  print(e,"chetnoe chislo")
+else: print(e, "nechotnoe chislo")
