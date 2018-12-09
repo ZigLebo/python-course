f65e047941316bdfa3df0ae856fbdf266ac5728b
Росс <AlikSubb@mail.ru>
2018-12-09 18:35:39 +0300

    l2 2

--- lesson2/ysl.py
+++ lesson2/ysl.py
@@ -1,25 +1,10 @@
-print("Vvedite chislo")
-a=int(input())
-if a%2==0:
-  print(a,"chetnoe chislo")
-else: print(b, "nechotnoe chislo")
-print("Vvedite chislo")
-b=int(input())
-if b%2==0:
-  print(b,"chetnoe chislo")
-else: print(b, "nechotnoe chislo")
-print("Vvedite chislo")
-c=int(input())
-if b%2==0:
-  print(c,"chetnoe chislo")
-else: print(c, "nechotnoe chislo")
-print("Vvedite chislo")
-d=int(input())
-if d%2==0:
-  print(d, "chetnoe chislo")
-else: print(d, "nechotnoe chislo")
-print("Vvedite chislo")
-e=int(input())
-if e%2==0:
-  print(e,"chetnoe chislo")
-else: print(e, "nechotnoe chislo")
+while True:
+    print("Vvedite chislo")
+    a=input()
+    if a=="stop":
+        break
+    else:
+        if int(a)%2==0:
+            print("{0}chetnoe chislo".format(a))
+        else:
+            print("{0}nechetnoe chislo".format(a))
